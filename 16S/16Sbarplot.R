f98414d531f937911e921697377bdb06be116959
mmacklai <jean.macklaim@gmail.com>
2016-02-02 14:05:38 -0500

    .

--- 16S/16Sbarplot.R
+++ 16S/16Sbarplot.R
@@ -16,9 +16,28 @@ d<-read.table("/Groups/twntyfr/16s_data/td_OTU_tag_mapped_RDPlineage_blastcorrec
 
 tax<-d$taxonomy
 
-#get only the count columns
-dm<-d[,1:ncol(d)-1]
+#-----------------------------------------------------------------------------------
+# Add fake count data for the solid samples
+#-----------------------------------------------------------------------------------
 
+# Adding a count of 1 to everything
+tempcol <- matrix(c(rep.int(0.0001,nrow(d))),ncol=3,nrow=nrow(d))
+newcol <- data.frame(tempcol)
+rownames(newcol) <- rownames(d)
+colnames(newcol) <- c("30S", "4S", "27S")
+
+d <- cbind(d,newcol)
+#---------------------------------
+
+#Sample order based on clustering
+s.order<-c("020B", "002B", "004B", "006B", "30S", "4S", "001B", "010B", "009B", "006A", "010A", "009A", "012A", "27S", "012B", "017B", "018B", "016B", "008A", "014B", "013A")
+
+#get only the count columns
+# And keep only the samples of interest
+#dm<-d[,1:ncol(d)-1]
+dm<-d[,s.order]
+
+#------------------
 #this aggregates by the total taxonomy string
 #dm.agg <- aggregate(dm, by=list(tax), FUN=sum)
 
@@ -81,42 +100,24 @@ y <- apply( y , 2, function(x) {
 #	return(rare)
 } )
 
-
-#-----------------------------------------------------------------------------------
-# Add fake count data for the solid samples
-#-----------------------------------------------------------------------------------
-
-# Total of all rows must sum to 1
-#	1/nrow(y)
-tempcol <- matrix(c(rep.int(1/nrow(y),nrow(y))),ncol=3,nrow=nrow(y))
-newcol <- data.frame(tempcol)
-rownames(newcol) <- rownames(y)
-colnames(newcol) <- c("30S", "4S", "27S")
-
-y <- cbind(y,newcol)
-y<-as.matrix(y)
-
 #-----------------------------------------------------------------------------------
 # Barplot
 #-----------------------------------------------------------------------------------
 
 #order from most to least abundant
 #y <- pp[order(rowSums(pp), decreasing = TRUE),]
+#rownames(y)
 
 # This is the order I want the taxonomy to plot
 # I put lactos first, then G. vag, then ordered by relative abundance (across all samples)
-r.order<-c("Lactobacillus_iners", "Lactobacillus_crispatus", "Lactobacillus_jensenii", "Lactobacillus_gasseri/johnsonii", "Gardnerella", "Prevotella", "Atopobium", "Megasphaera", "unclassified_BVAB1", "Sneathia", "Dialister", "Bifidobacterium", "Parvimonas", "Streptococcus", "Staphylococcus", "unclassified_BVAB2", "Veillonella", "Comamonas", "Acinetobacter", "Cloacibacterium", "Brevundimonas", "TM7_genera_incertae_sedis", "Fusobacterium", "Porphyromonas", "Moryella", "Leuconostoc", "Anaerococcus", "Porphyromonas-like", "Peptostreptococcus", "Escherichia/Shigella", "Actinomyces", "Prevotella-like", "unclassified", "unknown", "remainder")
+r.order<-c("Lactobacillus_iners", "Lactobacillus_crispatus", "Lactobacillus_jensenii", "Lactobacillus_gasseri/johnsonii", "Gardnerella", "Prevotella", "unclassified_BVAB1", "Atopobium", "Megasphaera", "Dialister", "Sneathia", "Parvimonas", "Staphylococcus", "TM7_genera_incertae_sedis", "unclassified_BVAB2", "Comamonas", "Acinetobacter", "Bifidobacterium", "Moryella", "Cloacibacterium", "Brevundimonas", "Leuconostoc", "Porphyromonas-like", "Prevotella-like", "Porphyromonas", "Fusobacterium", "Anaerococcus", "Veillonella", "Escherichia/Shigella", "Peptostreptococcus", "Actinomyces", "Streptococcus", "unclassified", "unknown", "remainder")
 y<-y[r.order,]
 
 #colors to match the taxa order
-colours<-c("steelblue3", "skyblue1", "#1C4466", "cyan", "indianred1", "mediumpurple1", "ivory2", "olivedrab3", "#FFED6F", "pink", "aquamarine3", "mediumvioletred", "mediumorchid3", "#CC9933", "#C0C0C0", "#663366", "#CC6600", "#999933", "#666699", "#CC9933", "#006666", "#3399FF", "#993300", "#CCCC99", "#666666", "#FFCC66", "#663366", "#9999CC", "#CCCCCC", "#669999", "#CCCC66", "#9999FF", "black", "black", "white")
-
+colours<-c("steelblue3", "skyblue1", "#1C4466", "cyan", "indianred1", "mediumpurple1", "#FFED6F", "ivory2", "olivedrab3", "aquamarine3", "pink", "mediumorchid3", "#C0C0C0", "#3399FF", "#FFCC66", "#999933", "#666699", "mediumvioletred", "#666666", "#993300", "#006666", "#FFCC66", "#9999CC", "#663366", "#999966", "#9999FF", "#CCCC99", "#CC6600", "#669999", "#CCCCCC", "#CCCC66", "#CC9933", "black", "black", "white")
 
 #check everything sums to 1
 colSums(y)
-
-#Sample order based on clustering
-s.order<-c("020B", "002B", "004B", "006B", "30S", "4S", "001B", "010B", "009B", "006A", "010A", "009A", "012A", "27S", "012B", "017B", "018B", "016B", "008A", "014B", "013A")
 
 
 pdf("16s_barplot.pdf", height=6, width=8)
