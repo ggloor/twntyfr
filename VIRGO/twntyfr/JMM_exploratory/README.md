0ad6663ac69896c5c18fab0ecb20475ad5c97efc
ggloor <ggloor@uwo.ca>
2020-02-11 08:19:55 -0500

    merged Jean's

--- VIRGO/twntyfr/JMM_exploratory/README.md
+++ VIRGO/twntyfr/JMM_exploratory/README.md
@@ -1,3 +1,3 @@
-JMM
-
-Playing around with the data, trying to make some pretty plots
+version https://git-lfs.github.com/spec/v1
+oid sha256:b99ece7a7d882f1d9ba433dcc61455d5638d5a024504157e167aeb24048069a4
+size 68
--- VIRGO/twntyfr/JMM_exploratory/load_packages.r
+++ VIRGO/twntyfr/JMM_exploratory/load_packages.r
@@ -1,19 +1,3 @@
-# Load standard packages for analyses
-# Jean M Macklaim
-
-library(MicrobeR)	# Functions for microbiome analyses: filtering, plotting barplots
-library(zCompositions) # Only needed for CLR zero estimates (CZM method) if NOT using ALDEx2 estimates
-library(ALDEx2)	# For CLR transformations and differential expression analyses
-library(ape)	# Phylogenetic plots (dendrograms)
-library(tidyverse)
-library(ggplot2)	# Figure generation
-library(dplyr)	# Data manipulation in tidyverse
-library(gplots)
-library(RColorBrewer)	# Plotting colorschemes
-library(phyloseq)	# Microbiome analyses
-library(CoDaSeq)	# Functions for compositonal data anlyses
-library(cowplot)	# For combining multiple ggplot figures
-
-
-# [ ] Write quick descriptions for what each package is used for
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:404ec43914f85950d85d0874bffce645feb5e6a97702c9c0ee7440607f0fed11
+size 761
