65fa7bbe5d28eaa1271eccb6c8a6039fee361e99
ggloor <ggloor@uwo.ca>
2020-03-24 13:47:18 -0400

    started functions and color list

---
+++ VIRGO/code/0_README.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:830776bd5b227267b1b28465bc1a78b4f02a9a0f5590547c0a07bae940230bca
+size 126
---
+++ VIRGO/code/setup.R
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0d416bccd88f0d9cfef3460ee21cd6920165ff640142cef6ec718bed9ad16a45
+size 166
