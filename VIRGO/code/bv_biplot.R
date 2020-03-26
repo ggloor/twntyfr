453e9ab64e25da5a03cfaa60fa36f55df2b471c5
ggloor <ggloor@uwo.ca>
2020-03-26 14:10:17 -0400

    added code and bv only biplot

---
+++ VIRGO/code/bv_biplot.R
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a8c2f2746bd0c16e57797fd1a0354865712d7432b75b3e35263b692e3a657d7a
+size 1450
--- VIRGO/code/merge_table.R
+++ VIRGO/code/merge_table.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8788848b99fcfc4f67926de5ef9210c3a7412e52718ed5dd9478865e9f959e9e
-size 1777
+oid sha256:11146c3985957614f251b367ca79ed9debbcdf6ece5f3716f53521a0c3cb469a
+size 2140
