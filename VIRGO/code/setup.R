a17f76ba29262a3182d00daddd525d02a682df04
ggloor <ggloor@uwo.ca>
2021-05-19 10:07:31 -0400

    added combined metadata
    only age, ph, nugent score

--- VIRGO/code/setup.R
+++ VIRGO/code/setup.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1cc72920ea88fa362fb8a3a4741edfaeffe535b7cd86af4792af432b77abbe9
-size 6624
+oid sha256:9ceb2a721b20f1a58eec04b4fefcb25244858794b25f8882980c5e6e2812d401
+size 7210
